2588f8620ea2e125fc8b05141dbe628e86b4fabe
Pierre Mehaye <pierre.753@hotmail.fr>
2024-09-16 12:25:46 +0200

    Added dummy test and README

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Running
+
+You need `Cargo` installed. Build the project:
+
+```shell
+$ cargo build
+```
+
+Run the tests:
+
+```shell
+$ cargo test
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dummy_test() {
+        main();
+        assert_eq!(true,true);
+    }
+}
